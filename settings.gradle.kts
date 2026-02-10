47d64fc1c5634487dc36bd646b947f5e3d2841da
ssricardo <rss.ricardo@gmail.com>
2026-02-10 19:04:19 +0100

    Define missing repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,21 @@ pluginManagement {
     plugins {
         kotlin("jvm") version "2.2.0"
     }
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
 }
 
 rootProject.name = "KistORM"
